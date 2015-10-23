1b0ce8c972367db8225aa80278c548cdf02dc323
Jack Lindamood <jack@signalfx.com>
2015-10-22 17:56:45 -0700

    More helper install tools

---
+++ scripts/install_lint_tools.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+set -e
+if [ -z "$CIRCLECI" ]; then
+	echo "Very likely you only want to run this setup step on circleCI"
+	exit 1
+fi
+
+if [ -z "$CACHED_LINT_TOOLS_DIR" ]; then
+	echo "Set cached lint tools to the directory you want to install this cache into"
+	exit 1
+fi
+
+export GOPATH="$HOME/tmp_gopath"
+
+mkdir -p "$CACHED_LINT_TOOLS_DIR"
+cd "$INSTALL_INTO"
+go get -u github.com/cep21/gobuild
+gobuild install
+cp "$GOPATH/bin/*" "$CACHED_LINT_TOOLS_DIR/"
+rm -rf "$GOPATH"
+cd -
---
+++ scripts/run_ci_tests.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd "$SRC_PATH"
+gobuild -verbose
+cd -
