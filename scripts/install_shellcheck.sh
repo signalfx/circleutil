12a8657dbb6fe1aa9b3a5e09f923505cbbdbb7a1
Jack Lindamood <jack@signalfx.com>
2015-10-26 17:12:20 -0700

    Fix shellcheck install glob

--- scripts/install_shellcheck.sh
+++ scripts/install_shellcheck.sh
@@ -3,8 +3,9 @@ set -x
 (which shellcheck &> /dev/null)
 if [ $? -ne 0 ]; then
 	set -e
-	wget http://ftp.debian.org/debian/pool/main/s/shellcheck/shellcheck_0.3.7-4_amd64.deb
+	SHELLCHECK_VERSION="0.3.7-4"
+	wget http://ftp.debian.org/debian/pool/main/s/shellcheck/shellcheck_${SHELLCHECK_VERSION}_amd64.deb
 	mkdir "$HOME/shellcheck"
-	dpkg -x "shellcheck_0.3.7-1_amd64.deb $HOME/shellcheck"
+	dpkg -x shellcheck_${SHELLCHECK_VERSION}_amd64.deb "$HOME/shellcheck"
 	cp "$HOME/shellcheck/bin/shellcheck $CACHED_LINT_TOOLS_DIR"
 fi
