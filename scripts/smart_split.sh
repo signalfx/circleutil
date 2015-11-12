e38c087e7a3bd79c3afd13989468887c08c5e870
Jack Lindamood <jack@signalfx.com>
2015-11-11 22:14:41 -0800

    Fix smart split

--- scripts/smart_split.sh
+++ scripts/smart_split.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 set -ex
+
+function print_time() {
+  echo "==PRINT_TIME $(date +%s) $(date) == $1"
+}
+
 which python
+which junitappend
+which cat
+
 EXIT_CODE=0
-LOOP_PARTS=$(cat | junitappend split)
-for d in $LOOP_PARTS;
+while read line
 do
-  print_time "Running test $d"
+  print_time "Running test $line"
   START_TIME=$(python -c 'import time;print time.time()')
   DID_CMD_WORK="0"
   FAILURE_MSG=""
-  $d || DID_CMD_WORK="1"
+  $line || DID_CMD_WORK="1"
   if [ "$DID_CMD_WORK" -ne "0" ]; then
-    FAILURE_MSG="Failed to run command $d"
+    FAILURE_MSG="Failed to run command $line"
     EXIT_CODE=1
   fi
   END_TIME=$(python -c 'import time;print time.time()')
   TOTAL_TIME=$(python -c 'import sys;print float(sys.argv[2]) - float(sys.argv[1])' "$START_TIME" "$END_TIME")
-  junitappend -testname "$d" -testduration "${TOTAL_TIME}s" -failuremsg "$FAILURE_MSG" add
-done
+  junitappend -testname "$line" -testduration "${TOTAL_TIME}s" -failuremsg "$FAILURE_MSG" add
+done < "$(cat | junitappend split)"
 exit $EXIT_CODE
