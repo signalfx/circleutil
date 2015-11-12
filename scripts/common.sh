d771aeb54a092073f4f990c1aa84365868c641f1
Jack Lindamood <jack@signalfx.com>
2015-11-12 08:11:58 -0800

    Better as a bash function

--- scripts/common.sh
+++ scripts/common.sh
@@ -77,3 +77,31 @@ function docker_release_tag() {
   fi
   echo -n "$DOCKER_TAG"
 }
+
+# Splits stdin and executes each line.
+# Intended to be used as a subshell with stdin piped in.
+# By doing it as a subshell with stdin piped in rather than xargs I avoid
+# some of the xargs # of values limitations.
+function speed_split() {
+  which python
+  which junitappend
+  which cat
+
+  EXIT_CODE=0
+  while read line
+  do
+    print_time "Running test $line"
+    START_TIME=$(python -c 'import time;print time.time()')
+    DID_CMD_WORK="0"
+    FAILURE_MSG=""
+    $line || DID_CMD_WORK="1"
+    if [ "$DID_CMD_WORK" -ne "0" ]; then
+      FAILURE_MSG="Failed to run command $line"
+      EXIT_CODE=1
+    fi
+    END_TIME=$(python -c 'import time;print time.time()')
+    TOTAL_TIME=$(python -c 'import sys;print float(sys.argv[2]) - float(sys.argv[1])' "$START_TIME" "$END_TIME")
+    junitappend -testname "$line" -testduration "${TOTAL_TIME}s" -failuremsg "$FAILURE_MSG" add
+  done < <(cat | junitappend split)
+  return $EXIT_CODE
+}
